be433ba5087d2a1a05bc8fc8749067e7c85bfb6b
João Lucas Saldanha <jlucassaldanha@gmail.com>
2026-08-14 15:54:56 -0300

    feat: adicionando cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import basicRoutes from './routes/basicRoutes.js'
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/api', basicRoutes);
