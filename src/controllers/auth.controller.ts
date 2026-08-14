aa34edffb75c47348381c8be1bc17803544b405d
João Lucas Saldanha <jlucassaldanha@gmail.com>
2026-08-14 17:16:15 -0300

    fix: Adicionando rota e corrigindo controller
    Criando classe do controller auth

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -4,32 +4,34 @@ import { AuthService } from '../services/auth.service';
 
 const authService = new AuthService()
 
-export const registerUser = async (req: Request, res: Response) => {
-  try {
-    const { email, name, password, role } = req.body;
+export class AuthController {
+  async register(req: Request, res: Response) {
+    try {
+      const { email, name, password, role } = req.body;
 
-    if (!email || !name || !password || !role) {
-      return res.status(400).json({ error: "Todos os campos são obrigatórios"})
+      if (!email || !name || !password || !role) {
+        return res.status(400).json({ error: "Todos os campos são obrigatórios"})
+      }
+
+      const validRoles: UserRole[] = ["ORGANIZADOR", "CLIENTE", "PORTARIA"];
+      if (!validRoles.includes(role.toUpperCase() as UserRole)) {
+        return res.status(400).json({ error: "Cargo inválido." })
+      }
+
+      const user = await authService.register({
+        name, 
+        email, 
+        password, 
+        role: role.toUpperCase() as UserRole
+      })
+
+      return res.status(201).json(user)
+    } catch (error: any) {
+      if (error.message === "EMAIL_ALREADY_EXISTS") {
+        return res.status(400).json({ error: "Este e-mail já está em uso."})
+      }
+
+      return res.status(500).json({ error: 'Erro interno do servidor.' })
     }
-
-    const validRoles: UserRole[] = ["ORGANIZADOR", "CLIENTE", "PORTARIA"];
-    if (!validRoles.includes(role.toUpperCase() as UserRole)) {
-      return res.status(400).json({ error: "Cargo inválido." })
-    }
-
-    const user = await authService.register({
-      name, 
-      email, 
-      password, 
-      role: role.toUpperCase() as UserRole
-    })
-
-    return res.status(201).json(user)
-  } catch (error: any) {
-    if (error.message === "EMAIL_ALREADY_EXISTS") {
-      return res.status(400).json({ error: "Este e-mail já está em uso."})
-    }
-
-    return res.status(500).json({ error: 'Erro interno do servidor.' })
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import basicRoutes from './routes/basicRoutes.js'
 import cors from 'cors';
+import authRouter from './routes/auth.routes.js';
 
 const app = express();
 const PORT = process.env.PORT;
@@ -8,7 +9,7 @@ const PORT = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 
-app.use('/api', basicRoutes);
+app.use('/api/auth', authRouter);
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta:${PORT}`)
